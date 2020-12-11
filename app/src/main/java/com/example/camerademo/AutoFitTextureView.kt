573ba73679c29bf95dd673a9523dfdeced9b46db
mobcoder-nikhil-singh <nikhil.vadhva@mobcoder.com>
2020-12-11 21:15:56 +0530

    First commit for base project

---
+++ app/src/main/java/com/example/camerademo/AutoFitTextureView.kt
@@ -0,0 +1,4 @@
+package com.example.camerademo
+
+class AutoFitTextureView {
+}
---
+++ app/src/main/java/com/example/camerademo/CameraControllerPreview.kt
@@ -0,0 +1,12 @@
+package com.example.camerademo
+
+import android.app.Activity
+
+class CameraControllerPreview(activity: Activity, textureView: AutoFitTextureView) {
+
+    companion object
+    {
+
+    }
+
+}
